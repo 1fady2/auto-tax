cd94022ccaf3895e3decddcad7ca4942c2f6bc67
1fady2 <controlre89@gmail.com>
2024-07-27 17:12:09 +0300

    ddddd
    dddddddd

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) => {
+  res.send('Hello Express app!')
+})
+
+app.use('/ping', (req, res) => {
+  res.send(new Date());
+});
+
+app.listen(3000, () => {
+  console.log('Express is ready.')
+});
+let autotax = ['1037441579432030229'];
+
+client.on("messageCreate", message => {
+ if(message.channel.type === "dm" || 
+  message.author.bot) return
+
+if(autotax.includes(message.channel.id)){
+
+  var args = message.content.split(' ').slice(0).join(' ')
+if(!args) return;
+
+if (args.endsWith("m")) args = args.replace(/m/gi, "") * 1000000;
+else if (args.endsWith("k")) args = args.replace(/k/gi, "") * 1000;
+else if (args.endsWith("K")) args = args.replace(/K/gi, "") * 1000;
+else if (args.endsWith("M")) args = args.replace(/M/gi, "") * 1000000;
+    let args2 = parseInt(args)
+    let tax = Math.floor(args2 * (20) / (19) + (1))
+    let tax2 = Math.floor(args2 * (20) / (19) + (1)-(args2))
+
+    let embed = new MessageEmbed()
+
+.setAuthor(`TAX CALCULATOR`, client.user.avatarURL({ dynamic: true }))
+
+   .setThumbnail(client.user.avatarURL({ dynamic: true }))   
+
+.addFields([
+    {
+    name: `Amount: `,
+    value: `${tax}`
+},
+  {
+    name: `Tax: `, 
+    value: `${tax2}`
+  }
+])
+        .setColor(message.guild.me.displayColor)
+  .setTimestamp()
+
+   message.reply({embeds: [embed]}).catch((err) => {
+   console.log(err.message)
+   });    
+  }
+}); 
+chalk 
+client.login("MTI2NjU0MTMzNTA5MjAwNjk1Mw.G1Sm1H.FydpsIFSfEHW2Qoc6GINS6LxwaXHiDoDNzfi8o");
